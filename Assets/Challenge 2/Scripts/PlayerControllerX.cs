60a2a5f94807fcfe663a594c6be1f337874e6c58
Jordan0023 <jordanmagdalena0023@gmail.com>
2023-10-16 21:42:29 -0700

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float dogWait;
+
+    private const float TimeBetweenDog = 1.0f;
+
+    private bool spawnDog = false;
+
+    void Update()
+    {
+
+        if (dogWait > 0)
+        {
+            dogWait -= Time.deltaTime;
+        }
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            if (dogWait <= 0)
+            {
+                dogWait = TimeBetweenDog;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+            
+        }
+    }
+}
